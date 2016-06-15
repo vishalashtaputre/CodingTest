d617668ea326d7aa5c5687a7c883dcce96e31ba3
vishalashtaputre <vishal.ashtaputre@gmail.com>
2016-06-14 23:07:12 -0700

    Update Problem4_Match.java

--- Problem4_Match.java
+++ Problem4_Match.java
@@ -4,7 +4,8 @@ import java.util.Set;
 public class Problem4_Match {
 	/*
 	* Checks whether the given Person objects are equal or not. It also checks whether the given objects are unique or not.
-	* The uniqueness is checked by adding the given Person objects to HashSet and checking whether the size of HashSet is two or not.
+	* The uniqueness is checked by adding the given Person objects to HashSet and checking whether the size of HashSet is one or not. 
+	* If the size of HashSet is one it means they both are duplicates and HashSet has only unique Person Objects.
 	* 
 	* The Person class needs to override implementation of equals and hashcode methods to perform this method correctly. See Person class for implementation.
 	*/
@@ -18,7 +19,7 @@ public class Problem4_Match {
 		setPerson.add(p2);
 		Boolean isUnique = Boolean.FALSE;
 		
-		if(setPerson.size() == 2){
+		if(setPerson.size() == 1){
 			isUnique = Boolean.TRUE;
 		}
 		
